6fa677a945d620ebeec597db05a83514807080f8
nisharthk <36231811+nisharthk@users.noreply.github.com>
2018-02-19 11:06:49 +0530

    added formatted code

--- Custom Exception/src/com/customException/NumberRangeException.java
+++ Custom Exception/src/com/customException/NumberRangeException.java
@@ -1,10 +1,8 @@
 package com.customException;
 
 @SuppressWarnings("serial")
-class NumberRangeException extends Exception
-{
-	NumberRangeException()
-	{
+class NumberRangeException extends Exception {
+	NumberRangeException() {
 		@SuppressWarnings("unused")
 		String msg = new String("Please Enter a number between 0 and 100");
 	}
