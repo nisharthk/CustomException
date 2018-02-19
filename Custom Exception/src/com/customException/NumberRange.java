8e62fad242ee9e4225e0b55a138186d098c158d6
nisharthk <36231811+nisharthk@users.noreply.github.com>
2018-02-19 11:06:17 +0530

    Added formatted code

--- Custom Exception/src/com/customException/NumberRange.java
+++ Custom Exception/src/com/customException/NumberRange.java
@@ -2,23 +2,20 @@ package com.customException;
 
 import java.util.Scanner;
 
-public class NumberRange
-{
-	public static void main (String args [ ])
-	{
-		try
-		{
-			System.out.println(" 	Custom Exception Example\n"); 
+public class NumberRange {
+	public static void main(String args[]) {
+		try {
+			System.out.println(" 	Custom Exception Example\n");
 			System.out.print("Enter a Number between 0 and 100 : ");
-			Scanner input=new Scanner(System.in);
-			int x=input.nextInt();
+			Scanner input = new Scanner(System.in);
+			int x = input.nextInt();
 			input.close();
-			if (x < 0 || x > 100) throw new NumberRangeException();
-			else System.out.println("\nThe number entered is within range!");
-		} 
-		catch (NumberRangeException e)
-		{
-			System.out.println("\nThe Number entered is not within range 0 and 100! "+e);
+			if (x < 0 || x > 100)
+				throw new NumberRangeException();
+			else
+				System.out.println("\nThe number entered is within range!");
+		} catch (NumberRangeException e) {
+			System.out.println("\nThe Number entered is not within range 0 and 100! " + e);
 		}
 	}
 }
